b4b519af30666ec1aebe06f8b9678c790b46fa69
Pamela Fox <pamela.fox@gmail.com>
2022-09-07 13:10:51 -0700

    Fixed indents

--- image_filters.py
+++ image_filters.py
@@ -5,8 +5,8 @@ def remove_red(pixel_data):
     """Sets all red channels to 0 in a list of pixels."""
     new_pixel_data = copy.deepcopy(pixel_data)
     for row in new_pixel_data:
-      for pixel in row:
-          pixel[0] = 0
+        for pixel in row:
+            pixel[0] = 0
     return new_pixel_data
 
 
@@ -14,8 +14,8 @@ def remove_green(pixel_data):
     """Sets all green channels to 0 in a list of pixels."""
     new_pixel_data = copy.deepcopy(pixel_data)
     for row in new_pixel_data:
-      for pixel in row:
-          pixel[1] = 0
+        for pixel in row:
+            pixel[1] = 0
     return new_pixel_data
 
 
